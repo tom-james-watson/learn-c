7f38b830e74ced50f3acb0c3b1f853b64f7cfdc1
Tom Watson <tom@tomjwatson.com>
2018-05-27 21:06:46 +0200

    ex8

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+  int i = 0;
 
-  int distance = 100;
-  float power = 2.345f;
-  double super_power = 56789.4532;
-  char initial = 'J';
-  char first_name[] = "Tom";
-  char last_name[] = "Watson";
+  if (argc == 1) {
+    printf("You only have one argument. You suck.\n");
+  } else if (argc > 1 && argc < 4) {
+    printf("Here's you arguments:\n");
 
-  printf("You are %d miles away.\n", distance);
-  printf("You have %f levels of power.\n", power);
-  printf("You have %f awesome super powers.\n", super_power);
-  printf("I have an initial %c.\n", initial);
-  printf("My first name is %s.\n", first_name);
-  printf("My last name is %s.\n", last_name);
-  printf("My full name is %s %c %s.\n", first_name, initial, last_name);
+    for (i = 0; i < argc; i++) {
+      printf("%s ", argv[i]);
+    }
 
-  int bugs = 100;
-  double bug_rate = 1.2;
-
-  printf("you have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
-  printf("you have %d bugs at the imaginary rate of %.2f.\n", bugs, bug_rate);
-
-  long universe_of_defects = 1L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L;
-  printf("The entire universe has %ld bugs.\n", universe_of_defects);
-
-  double expected_bugs = bugs * bug_rate;
-  printf("You are expected to have %f bugs.\n", expected_bugs);
-
-  double part_of_universe = expected_bugs / universe_of_defects;
-  printf("That is only a %e portion of the universe.\n", part_of_universe);
-
-  // this makes no sense, just a demo of something weird
-  char nul_byte = '\0';
-  int care_parcentage = bugs * nul_byte;
-  printf("Which means you should care %d%%.\n", care_parcentage);
+    printf("\n");
+  } else {
+    printf("You have too many arguments. You suck.\n");
+  }
 
   return 0;
-
 }
